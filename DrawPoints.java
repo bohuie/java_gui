0075c01b35ddcf9bc115bae9418a2614b2cac052
Bowen Hui <bohuie@gmail.com>
2013-05-20 14:31:04 -0700

    refactored some code

--- DrawPoints.java
+++ DrawPoints.java
@@ -37,18 +37,19 @@ class DrawPanel extends JPanel
     Graphics2D g2d = ( Graphics2D )g;
     g2d.setColor( Color.blue );
 
+    Dimension size   = getSize();
+    Insets    insets = getInsets();
+
+    int w = size.width  - insets.left - insets.right;
+    int h = size.height - insets.top  - insets.bottom;
+
+    Random r = new Random();
+
     // randomly paint a bunch of points within the window boundary
     for (int i = 0; i<=1000; i++) 
     {
-      Dimension size   = getSize();
-      Insets    insets = getInsets();
-
-      int w = size.width  - insets.left - insets.right;
-      int h = size.height - insets.top  - insets.bottom;
-
-      Random r = new Random();
-      int    x = Math.abs( r.nextInt() ) % w;
-      int    y = Math.abs( r.nextInt() ) % h;
+      int x = Math.abs( r.nextInt() ) % w;
+      int y = Math.abs( r.nextInt() ) % h;
       g2d.drawLine( x, y, x, y );       // effectively draws a point
     }
   }
